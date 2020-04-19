6a1ae13a7af7a39f78dd214f96253da7bdd2a6ec
bagietMajster <kamil.lenart@op.pl>
2020-04-19 19:59:09 +0200

    Buttons

--- sciCalculator/MainWindow.xaml.cs
+++ sciCalculator/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace sciCalculator
         {
             InitializeComponent();
         }
+
+        private void OnSimpleCharacterClick(object sender, RoutedEventArgs e)
+        {
+            var buttonValue = ((Button)sender).Content;
+            enterTextBox.Text += buttonValue.ToString();
+        }
+
+        private void Clean(object sender, RoutedEventArgs e)
+        {
+            enterTextBox.Text = String.Empty;
+        }
+
+        private void Sum(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var buttonValue = ((Button)sender).Content;
+
+            switch (buttonValue)
+            {
+                case "π":
+                    enterTextBox.Text += "π";
+                    return;
+                case "x^y":
+                    enterTextBox.Text += "(x^y)";
+                    return;
+                case "Factorial":
+                    enterTextBox.Text += "(!x)";
+                    return;
+                case "log":
+                    enterTextBox.Text += "log(x)(y)";
+                    return;
+                case "ln":
+                    enterTextBox.Text += "ln(x)";
+                    return;
+                case "e":
+                    enterTextBox.Text += "e";//????
+                    return;
+                default:
+                    break;
+            }
+
+            enterTextBox.Text += buttonValue.ToString();
+        }
     }
 }
